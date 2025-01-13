24af166bd8175ea21b21676d1417dfdce35e3e07
Aryanlivi <aryanlivi@gmail.com>
2025-01-13 16:55:45 +0545

    Inital DB setup

---
+++ DB_Service/InitialDB_Setup.py
@@ -0,0 +1,36 @@
+from Database import Database
+from dotenv import load_dotenv
+
+load_dotenv()
+
+galchi_table = 'Forecast_Galchi_To_Siurenitar'
+budhi_table = 'Forecast_Budhi_At_Khari_To_Siurenitar'
+siurenitar_table = 'Forecast_Siurenitar_Data'
+db = Database()
+
+
+db.connect()
+
+
+db.execute_query(f'''
+    CREATE TABLE IF NOT EXISTS public.{galchi_table} (
+    dateTime TIMESTAMP WITHOUT TIME ZONE UNIQUE,
+    discharge FLOAT
+    );
+
+    CREATE TABLE IF NOT EXISTS public.{budhi_table} (
+        dateTime TIMESTAMP WITHOUT TIME ZONE UNIQUE,
+        discharge FLOAT
+    );
+
+    CREATE TABLE IF NOT EXISTS public.{siurenitar_table} (
+        dateTime TIMESTAMP WITHOUT TIME ZONE UNIQUE,
+        discharge FLOAT
+    );
+
+''')
+
+    
+
+
+db.disconnect()
